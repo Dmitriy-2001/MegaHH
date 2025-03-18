1887f50748fdf3b8e070cd61211ed8fa59574590
Dmitrii <richard.orlov13@gmail.com>
2025-03-18 16:48:27 +0300

    megaHH-4/version update

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -6,10 +6,10 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.diploma.data.Constants.BASE_URL_HH
+import ru.practicum.android.diploma.data.favorites.db.AppDatabase
 import ru.practicum.android.diploma.data.network.HHApi
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
-import ru.practicum.android.diploma.data.favorites.db.AppDatabase
 
 val dataModule = module {
     single<NetworkClient> {
