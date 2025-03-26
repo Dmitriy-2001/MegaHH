fe978b7a2bd4b205c7f3fe569a21ecd0ed17fac4
just_ann <a.kiritseva@kodington.ru>
2025-03-26 13:59:38 +0300

    megaHH-33. Пагинация + progress bar

--- app/src/main/java/ru/practicum/android/diploma/domain/search/impl/VacanciesInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/impl/VacanciesInteractorImpl.kt
@@ -8,7 +8,8 @@ import ru.practicum.android.diploma.domain.search.models.VacanciesModel
 import ru.practicum.android.diploma.domain.search.models.VacancyModel
 
 class VacanciesInteractorImpl(private val repository: VacanciesRepository) : VacanciesInteractor {
-    override fun searchVacancies(text: String): Flow<Resource<VacanciesModel>> = repository.searchVacancies(text)
+    override fun searchVacancies(text: String, page: Int?): Flow<Resource<VacanciesModel>> =
+        repository.searchVacancies(text, page)
 
     override fun getVacancyById(id: String): Flow<Resource<VacancyModel>> = repository.getVacancyDetailsById(id)
 }
